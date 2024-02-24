b605bf4e82702fb283416ab958c45df296b658aa
mayank-sharma-appinventiv-2 <138752838+mayank-sharma-appinventiv-2@users.noreply.github.com>
2024-02-24 23:15:53 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import React from "react"
+import Home from "./components/home"
+import Header from './components/header';
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header/>
+      <RouterProvider router={router} />
     </div>
   );
 }
